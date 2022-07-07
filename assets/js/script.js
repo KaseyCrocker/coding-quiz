d48bc24f5505387fef81a552ac07592c993e6b43
Kasey <kasey_crocker@yahoo.com>
2022-07-07 14:23:59 -0400

    quiz made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,180 @@
-var headerVar = document.getElementById("header");
-var countDownVar = document.getElementById("countdown");  // done
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 var btnStartVar = document.getElementById("btn-start");
-var initialsVar = document.getElementById("initials");
-var btn1Var = document.getElementById("btn1");
-var btn2Var = document.getElementById("btn2");
-var btn3Var = document.getElementById("btn3");
-var btn4Var = document.getElementById("btn4");
-var finalScoreVar = document.getElementById("final-score");
-var mainContainerVar = document.getElementById("mainContainer");
-var startQuizVar = document.getElementById("startQuiz")
-var time = 75;
+var startQuizVar = document.getElementById("startQuiz");
 var questionsArr = document.getElementById("showQuestions");
 
-console.log(countDownVar);
+function generateQuiz(){
+    var myQuestions = [
+        {
+            question: "What's the short cut to create a default page in HTML?",
+            answers: ["html:5", "HTML", "It does not exist", "html'5"],
+            correctAnswer: "0"
+        },
+        {
+            question: "What's the first line in a HTML document?",
+            answers: ["<docs html>","<!DOCTIPE html>", "<!DOCTYPE html>", "<html lang=>"],
+            correctAnswer: "2"
+        },
+        {
+            question: "Which answer is not true?",
+            answers: ["var number = x;", "let number = y;","var arrNumber = [];", "div function() {};"],
+            correctAnswer: "3"
+        },
+        {
+            question: "What's the attribute that indicates the destination of the hyperlink after the anchor <a ...?",
+            answers: ["ref", "href","img","id"],
+            correctAnswer: "1"
+        },
+        {
+            question: "How can you print a variable or a message in the debugging console in JavaScript?",
+            answers: ["console.ra();", "console.log();", "console.val();", "console.911();"],
+            correctAnswer: "1"
+        },
+        {
+            question: "What's the correct way to display an image in css?",
+            answers: ["<img...", "background-image:url('...')","padding: ...", "display-img"],
+            correctAnswer: "2"
+        },
+        {
+            question: "What DOM stands for in JavaScript?",
+            answers: ["Document Object Model", "Document Object Modify","Document Other Model", "Days On Market"],
+            correctAnswer: "0"
+        },
+        {
+            question: "How can you display the color inside this code :root {--primary-color=#fce138}?",
+            answers: ["var(primary-color);","var(--primary-color);","var(-primary-color);", "primary-color();"],
+            correctAnswer: "1"
+        },
+        {
+            question: "If you are in UCF BootCamp and you need help, what would be the first thing to do?",
+            answers: ["call 911","ask in BCS Learning Assistant","watch movies to destress myself", "wait until class"],
+            correctAnswer: "1"
+        },
+        {
+            question: "What's the default value for flex-direction?",
+            answers: ["column", "center","space-between", "row"],
+            correctAnswer: "3"
+        },
+    ];
 
+	function quiz(){
+		// we'll need a place to store the output and the answer choices
+	    var output = [];
 
-
-console.log(document.getElementsByTagName("title"));
-var questions = [
-    {
-        question: "What's the short cut to create a default page in HTML?",
-        options: ["html:5", "HTML", "It does not exist", "html'5"],
-        answer: 0
-    },
-    {
-        question: "What's the first line in a HTML document?",
-        options: ["<docs html>","<!DOCTIPE html>", "<!DOCTYPE html>", "<html lang=>"],
-        answer: 2
-    },
-    {
-        question: "Which option is not true?",
-        options: ["var number = x;", "let number = y;","var arrNumber = [];", "div function() {};"],
-        answer: 3
-    },
-    {
-        question: "What's the attribute that indicates the destination of the hyperlink after the anchor <a ...?",
-        options: ["ref", "href","img","id"],
-        answer: 1
-    },
-    {
-        question: "How can you print a variable or a message in the debugging console in JavaScript?",
-        options: ["console.ra();", "console.log();", "console.val();", "console.911();"],
-        answer: 1
-    },
-    {
-        question: "What's the correct way to display an image in css?",
-        options: ["<img...", "background-image:url('...')","padding: ...", "display-img"],
-        answer: 2
-    },
-    {
-        question: "What DOM stands for in JavaScript?",
-        options: ["Document Object Model", "Document Object Modify","Document Other Model", "Days On Market"],
-        answer: 0
-    },
-    {
-        question: "How can you display the color inside this code :root {--primary-color=#fce138}?",
-        options: ["var(primary-color);","var(--primary-color);","var(-primary-color);", "primary-color();"],
-        answer: 1
-    },
-    {
-        question: "If you are in UCF BootCamp and you need help, what would be the first thing to do?",
-        options: ["call 911","ask in BCS Learning Assistant","watch movies to destress myself", "wait until class"],
-        answer: 1
-    },
-    {
-        question: "What's the default value for flex-direction?",
-        options: ["column", "center","space-between", "row"],
-        answer: 3
-    },
-
-];
-
-
-function countDown (time){
-
-    var timeInterval = setInterval(function(){
+	    // for each question...
+	    myQuestions.forEach((currentQuestion, questionNumber) =>{
         
-        if(time > 0){
-            countDownVar.textContent = time;
-            time--;             
-        }
-        else {
-            alert("You are out of time!");
-        }
+	    	// first reset the list of answers
+	    	var answers = [];
 
-    }, 1000);
+	    	// for each available answer to this question...
+	    	for(letter in currentQuestion.answers){
+
+	    		// ...add an html radio button
+	    		answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+	    		);
+	    	}
+
+	    	// add this question and its answers to the output
+	    	output.push(
+                `<div class="slide">
+                    <div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join("")} </div>
+                </div>`
+		    );
+	    });
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
+	}
+
+	function showResults() {
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll(".answers");
+    
+        // keep track of user's answers
+        let numCorrect = 0;
+    
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // find selected answer
+            var answerContainer = answerContainers[questionNumber];
+            var selector = `input[name=question${questionNumber}]:checked`;
+            var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+            
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+            
+                // color the answers green
+                answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+        
+        if (currentSlide === 0) {
+            previousButton.style.display = "none";
+        } else {
+            previousButton.style.display = "inline-block";
+        }
+        
+        if (currentSlide === slides.length - 1) {
+            qnextButton.style.display = "none";
+            qsubmitButton.style.display = "inline-block";
+        } else {
+            nextButton.style.display = "inline-block";
+            submitButton.style.display = "none";
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    var quizContainer = document.getElementById("quiz");
+    var resultsContainer = document.getElementById("results");
+    var submitButton = document.getElementById("submit");
+
+    quiz();
+
+    var nextButton = document.getElementById("next");
+    var slides = document.querySelectorAll(".slide");
+    var currentSlide = 0;
+
+    showSlide(0);
+
+    // on submit, show results
+    submitButton.addEventListener("click", showResults);
+    nextButton.addEventListener("click", showNextSlide);
 }
+
 
 btnStartVar.addEventListener("click", function(){
 
-    // disable btn so the timer does not goes up and down in value, only strings
-    btnStartVar.setAttribute("disabled","true");
-    countDown(time);
-    // disable btn so the timer does not goes up and down in value, only strings
-    startQuizVar.setAttribute("class","hideElement"); 
+    btnStartVar.setAttribute("disabled","true"); // disable btn so the timer does not goes up and down in value, only strings
+    //countDown(time);
+    startQuizVar.setAttribute("class","hideElement"); // disable btn so the timer does not goes up and down in value, only strings
     questionsArr.removeAttribute("class");
-    arrayQuestions();
+    generateQuiz();
 });
 
-
-
-function arrayQuestions(questions){
-
-    var questionTitle = document.getElementById("title");
-
-    questionTitle.textContent = questions.question;
-//    for (var i = 0; i < questions.length; i++) {
-//        // ask question
-//        if (questions[i]) {
-//        questionTitle.textContent = questions.question;
-//        // once answered, display if answer was correct
-//    
-//        // loop to next question
-//        }
-//    }
-}
-
-
-console.log(questions);
-console.log(arrayQuestions);
